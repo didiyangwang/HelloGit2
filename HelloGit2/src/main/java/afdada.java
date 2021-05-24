1ce8da8953975b34e6ede56caf3744b33fc8e9f8
YangGe666 <158935706@qq.com>
2021-05-24 18:31:34 +0800

    弟弟2

--- HelloGit2/src/main/java/afdada.java
+++ HelloGit2/src/main/java/afdada.java
@@ -7,5 +7,7 @@ public class afdada {
     public static void main(String args[]) {
         System.out.println(1);
         System.out.println("分支更新");
+        System.out.println("分支添加2");
+
     }
 }
